71c85e2f4c002a649bc42698853b974952a1fa8b
Hoanh An <hoanhan101@gmail.com>
2023-10-25 23:14:53 -0700

    feat: snapshot assets

--- scripts/utils.py
+++ scripts/utils.py
@@ -39,30 +39,33 @@ def hex_to_felt(val):
 def prep_art():
     # header = '<svg id="leetart" width="100%" height="100%" viewBox="0 0 20000 20000" xmlns="http://www.w3.org/2000/svg"><style>#leetart{background-image:url('
     # footer = ");background-repeat:no-repeat;background-size:contain;background-position:center;image-rendering:-webkit-optimize-contrast;-ms-interpolation-mode:nearest-neighbor;image-rendering:-moz-crisp-edges;image-rendering:pixelated;}</style></svg>"
-    assets_dir = "./assets"
+    assets_dir = "./assets/kam_compressed_102523"
     for fn in os.listdir(assets_dir):
         if not fn.startswith("t"):
             continue
 
         with open(f"{assets_dir}/{fn}", "rb") as f:
             content = base64.b64encode(f.read()).decode(encoding="utf-8")
-            name = fn.split(".png")[0].split("_")[1].upper()
+            name = fn.split(".png")[0].split("_")[1]
             to_cairo(name, content)
 
 
 def to_cairo(name, content):
-    print(f"else if beast == {name}")
-    print("{")
+    print("#[inline(always)]")
+    print("fn get_" + name + "_svg() -> LongString {")
+    print("    let mut content = ArrayTrait::<felt252>::new();")
     for c in str_to_array(content):
         print(f"    content.append('{c}');")
 
+    print("    content.into()")
     print("}")
 
 
 if __name__ == "__main__":
+    # print(
+    #     str_to_array(
+    #         'data:image/svg+xml;utf8,<svg%20width="100%"%20height="100%"%20viewBox="0%200%2020000%2020000"%20xmlns="http://www.w3.org/2000/svg"><style>svg{background-image:url(data:image/png;base64,'
+    #     )
+    # )
+
     prep_art()
-    print(
-        str_to_array(
-            'data:image/svg+xml;utf8,<svg%20width="100%"%20height="100%"%20viewBox="0%200%2020000%2020000"%20xmlns="http://www.w3.org/2000/svg"><style>svg{background-image:url(data:image/png;base64,'
-        )
-    )
